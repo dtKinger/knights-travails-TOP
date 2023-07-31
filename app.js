15466a0ae277fb093e954cc11a552b5f0a0d411b
Dan King <king.daniel.thomas@gmail.com>
2023-07-31 12:48:43 +0700

    Return as x and y for now so I can name them during math

--- app.js
+++ app.js
@@ -13,14 +13,16 @@ for (let i = 0; i < chessBoard.length; i += 1){
   console.log(chessBoard[i])
 }
 
-// console.log(chessBoard[4][3])
+// Legal Knight Moves
+
 
 // Find WK1
 function getPosition(token){
   for (let i = 0; i < chessBoard.length; i += 1){
     for (let j = 0; j < chessBoard[i].length; j += 1){
       if (chessBoard[i][j] == token){
-        return `${token}'s position is ${i}, ${j}.`
+        console.log(`${token}'s position is ${i}, ${j}.`)
+        return {"x": i, "y": j}
       }
     }
   }
